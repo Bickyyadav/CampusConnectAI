3c643ca4022b19ac01f781f94152dd2390e314ec
Vicky Yadav <ybicky86@gmail.com>
2025-12-23 16:23:57 +0530

    some change in main.py

--- backend/main.py
+++ backend/main.py
@@ -47,9 +47,9 @@ from routers.health import health_router
 #     await check_scheduled_calls()
 
 
-# @app.on_event("startup")
-# async def startup():
-#     await init_db()
+@app.on_event("startup")
+async def startup():
+    await init_db()
     # await loader.start()
 
 
